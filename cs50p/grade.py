8a75c5b6380eb8a3d75d1c404d96cf17388cf41a
peacemaker564 <jappikaur17@gmail.com>
2025-09-27 19:45:52 +0000

    Sat, Sep 27, 2025, 1:45 PM -06:00

--- cs50p/grade.py
+++ cs50p/grade.py
@@ -1,9 +1,20 @@
-score = int(input("Whats your score? "))
 
 
+
+while True:
+    try:
+        score = int(input("Whats your score? "))
+        if 0 <= score <= 100:
+            break
+        else
+            print("Please enter a valid score(b/w 0-100)")
+
+    except valueError:
+        print("Invalid Input..please try entering a valid numbered score.")
+        
 if score > 100:
     print("Please, enter a valid score.(0-100)")
-    
+
 elif score >= 90:
     print("Outstanding performance..!")
 elif score >= 80:

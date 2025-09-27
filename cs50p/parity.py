1b372a29bedcb1db2916f090e2b117a85d03c7ba
peacemaker564 <jappikaur17@gmail.com>
2025-09-27 20:19:35 +0000

    Sat, Sep 27, 2025, 2:19 PM -06:00

--- cs50p/parity.py
+++ cs50p/parity.py
@@ -1,11 +1,13 @@
 #Program to check even or odd values.
+def main():
+    x = int(input("Whats x? "))
+    evenOdd(x)
 
-x = int(input("Whats x? "))
-
-if x % 2 == 0:
-    print("The number is even.")
-else:
-    print("The entered number is odd.")
-
+def evenOdd(num):
+    if num % 2 == 0:
+        print("The number is even.")
+    else:
+        print("The entered number is odd.")
 
 
+main()

cb3bf015861f8b5f8da62967ab8d59ff8b7a2737
peacemaker564 <jappikaur17@gmail.com>
2025-09-27 15:48:00 +0000

    Sat, Sep 27, 2025, 9:48 AM -06:00

---
+++ shows.py
@@ -0,0 +1,16 @@
+SHOWS = [
+    "Avatar: the last airbender",
+    "Ben 10",
+    "Arthur ",
+    " Spongebob squarepants",
+    "phineas and ferb",
+    "Kim possible",
+    "Jimmy Neutron",
+    "the Proud family"
+]
+
+def main():
+    for show in SHOWS:
+        print(show)
+
+main()

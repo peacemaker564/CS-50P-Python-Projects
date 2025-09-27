b36f664140cb8067804541251fec3dceb1e80b0a
peacemaker564 <jappikaur17@gmail.com>
2025-09-27 19:36:22 +0000

    Sat, Sep 27, 2025, 1:36 PM -06:00

---
+++ cs50p/grade.py
@@ -0,0 +1,14 @@
+score = int(input("Whats your score? "))
+
+if score > 90:
+    print("Outstanding performance..!")
+elif score > 80:
+    print("Appreciable")
+elif score > 70:
+    print("Good work")
+elif score > 60:
+    print("Could do better..")
+else:
+    print("You have failed.")
+
+

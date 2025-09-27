1a1d2ccae5eb239edb699d6e9dde6fb81bc1073f
peacemaker564 <jappikaur17@gmail.com>
2025-09-27 15:37:13 +0000

    Sat, Sep 27, 2025, 9:37 AM -06:00

--- cs50p/machine.py
+++ cs50p/machine.py
@@ -1,11 +1,14 @@
-emoticon = "v.v"
+emoticon = "v.v" #global variable
 
 def main():
+    global emoticon #allows us to change the global variable emoticon for a local main function.
     say("Is anyone there?")
-    
+    emoticon = ":D" #using global, this is not changeable./updatable.
+    say("Oh, hi !")
 
 
-
+#defined a function say.
 def say(phrase):
     print( phrase + " " + emoticon)
 
+main() #called the main function.

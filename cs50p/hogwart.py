858d32a21394bdbbda0a8a96073f021185822509
peacemaker564 <jappikaur17@gmail.com>
2025-10-16 19:49:02 +0000

    Thu, Oct 16, 2025, 1:49 PM -06:00

--- cs50p/hogwart.py
+++ cs50p/hogwart.py
@@ -1,5 +1,17 @@
 #creating a dictionary
-students = {"Hermione": "Gryffindor", "Harry":"Gryffindor" , "Ron":"Gryffindor" , "Draco" : "Slytherin"}
+students = {"Hermione": "Gryffindor",
+            "Harry":"Gryffindor" ,
+            "Ron":"Gryffindor" ,
+            "Draco" : "Slytherin"
+            }
 
-for student in students():
-    print(student)
+#This actually prints the keys of the dictionary.
+#for student in students:
+#   print(student)
+
+#To print out the values of the keys, we will do this.
+
+for student in students:
+    print(student, students[student], sep=",")
+
+

c9ea8dc6a516520c4bbcb7bf25ba36b9b4a7ed55
peacemaker564 <jappikaur17@gmail.com>
2025-10-16 19:25:43 +0000

    Thu, Oct 16, 2025, 1:25 PM -06:00

--- cs50p/hogwart.py
+++ cs50p/hogwart.py
@@ -5,7 +5,13 @@ students = ["Hermione", "Harry", "Ron"]
 
 #Perhaps, there is a better way to do this using loops.
 
-for student in students:
-    print(student) 
+#for student in students:
+    #print(student)
+
+
+#or even perhaps, more meaningful and true to what we learned in c++
+
+for i in range(len(students)):
+    print(i+1,".",students[i]) #will automatically increment the i variable, and print out.
 
 

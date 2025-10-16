b21152d4373fc33f439e068d0f554b4bc1a15292
peacemaker564 <jappikaur17@gmail.com>
2025-10-16 19:17:39 +0000

    Thu, Oct 16, 2025, 1:17 PM -06:00

---
+++ cs50p/hogwart.py
@@ -0,0 +1,11 @@
+#using lists
+students = ["Hermione", "Harry", "Ron"]
+
+#print(students[0]) ....#it goes on using list indices.
+
+#Perhaps, there is a better way to do this using loops.
+
+for student in students:
+    print(student) 
+
+

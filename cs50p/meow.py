7eef5cd908f7de368cc22cc8a2f398a7143f54cc
peacemaker564 <jappikaur17@gmail.com>
2025-10-16 19:03:36 +0000

    Thu, Oct 16, 2025, 1:03 PM -06:00

--- cs50p/meow.py
+++ cs50p/meow.py
@@ -1,5 +1,12 @@
 def main():
-    meow(3)
+    number = get_number()
+    meow(number)
+
+def get_number():
+    while True:
+        n = int(input("What the value of n? "))
+        if n > 0:
+            return n #This will automatically break out of the loop.
 
 def meow(n):
     for _ in range(n):

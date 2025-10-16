a1f3110c4d044236377b6d810f7d3f7deb959080
peacemaker564 <jappikaur17@gmail.com>
2025-10-16 19:46:40 +0000

    Thu, Oct 16, 2025, 1:46 PM -06:00

--- cs50p/hogwart.py
+++ cs50p/hogwart.py
@@ -1,17 +1,5 @@
-#using lists
-students = ["Hermione", "Harry", "Ron"]
+#creating a dictionary
+students = {"Hermione": "Gryffindor", "Harry":"Gryffindor" , "Ron":"Gryffindor" , "Draco" : "Slytherin"}
 
-#print(students[0]) ....#it goes on using list indices.
-
-#Perhaps, there is a better way to do this using loops.
-
-#for student in students:
-    #print(student)
-
-
-#or even perhaps, more meaningful and true to what we learned in c++
-
-for i in range(len(students)):
-    print(i+1,".",students[i]) #will automatically increment the i variable, and print out.
-
-
+for student in students():
+    print(student)

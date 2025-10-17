bddc6928b8398c939400d01f891a8e7080ecb83f
peacemaker564 <jappikaur17@gmail.com>
2025-10-17 04:42:30 +0000

    Thu, Oct 16, 2025, 10:42 PM -06:00

--- cs50p/hogwart.py
+++ cs50p/hogwart.py
@@ -3,7 +3,7 @@
 #           "Harry":"Gryffindor" ,
 #           "Ron":"Gryffindor" ,
 #            "Draco" : "Slytherin"
-            }
+#            }
 
 #This actually prints the keys of the dictionary.
 #for student in students:
@@ -11,8 +11,8 @@
 
 #To print out the values of the keys, we will do this.
 
-for student in students:
-    print(student, students[student], sep=",")
+#for student in students:
+#    print(student, students[student], sep=",")
 
 #Now, making sure we fully utilize keys in dictionaries and lists.
 students = [ {"Name" : "Hermione" , "House":"Gryffindor" , "Patronus":"Otter"} ,
@@ -22,4 +22,4 @@ students = [ {"Name" : "Hermione" , "House":"Gryffindor" , "Patronus":"Otter"} ,
 ]
 
 for student in students:
-        print(student["Name"])
+        print(student["Name"], student["House"], student["Patronus"], sep = " : ")

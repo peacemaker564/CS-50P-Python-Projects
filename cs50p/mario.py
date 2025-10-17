007d3cdff833bd98d81a4301187b3992860cd139
peacemaker564 <jappikaur17@gmail.com>
2025-10-17 05:00:21 +0000

    Thu, Oct 16, 2025, 11:00 PM -06:00

--- cs50p/mario.py
+++ cs50p/mario.py
@@ -1,6 +1,12 @@
 def main():
     n = int(input("Whats n? "))
     print_column(n)
+    print_row(n)
+
+
+def print_row(length):
+      for i in range(length):
+            print("?" )
 
 def print_column(height):
 #    for _  in range(height):
@@ -8,5 +14,6 @@ def print_column(height):
 
 #another way of writing this is print("#")
         print("#\n" * height, end = "")
+
 
 main()

db24bab16a59320f2920dd918944d3f9649593ff
peacemaker564 <jappikaur17@gmail.com>
2025-10-18 17:10:52 +0000

    Sat, Oct 18, 2025, 11:10 AM -06:00

--- cs50p/distances.py
+++ cs50p/distances.py
@@ -1,6 +1,11 @@
 distances = {
-    ""
-
-
+    "Voyager 1" : 163
+    "Voyager 2" : 136
+    "Pioneer 10" : 80
+    "New Horizons" : 58
+    "Pioneer 11" : 44
 
              }
+def main():
+    for name in distances.keys():
+        

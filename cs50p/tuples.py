65d96c9eadd5761180d00783e86b8cf685e07f82
peacemaker564 <jappikaur17@gmail.com>
2025-11-06 23:18:59 +0000

    Thu, Nov 6, 2025, 5:18 PM -06:00

--- cs50p/tuples.py
+++ cs50p/tuples.py
@@ -2,9 +2,18 @@ import sys
 
 def main():
     coordinates_tuples = (42.756, -71.115)
+    latitude =  coordinates_tuples[0]
+
+    longitude =  coordinates_tuples[1]
+    
     coordinates_lists = [42.756, -7.115]
-    print(coordinates_tuples)
+
+    print(f"Latitude = {latitude}")
+    print("\n")
+    print(f"Longitude = {longitude}")
+    print("\n")
     print(coordinates_lists)
+    print("\n")
     print(f"Tuples take {sys.getsizeof(coordinates_tuples)} bytes.")
     print(f"Lists take {sys.getsizeof(coordinates_lists)} bytes.")
 

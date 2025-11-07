d063bd8feaa987c1096077382dd6157611affe9a
peacemaker564 <jappikaur17@gmail.com>
2025-11-07 17:54:06 +0000

    Fri, Nov 7, 2025, 11:54 AM -06:00

--- cs50p/numbers.py
+++ cs50p/numbers.py
@@ -3,13 +3,15 @@ def main():
     print(f"x is {x}")
 
 def getint():
-    try:
-        x = int(input("What's the x? "))
-    except ValueError:
-        print("x is not an integer, dumbass.! Try again.")
 
-    else:
-        return x
+    while True:
+
+        try:
+            x = int(input("What's the x? "))
+        except ValueError:
+            print("x is not an integer, dumbass.! Try again.")
+        else:
+            return x
 
 
 main()

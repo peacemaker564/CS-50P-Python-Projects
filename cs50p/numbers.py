0f24cee852d35a4a676ee702f5ae9374a580db72
peacemaker564 <jappikaur17@gmail.com>
2025-11-07 04:42:23 +0000

    Thu, Nov 6, 2025, 10:42 PM -06:00

--- cs50p/numbers.py
+++ cs50p/numbers.py
@@ -2,7 +2,7 @@ try:
     x = int(input("What's the x? "))
     print(f"x is {x}")
 
-except:
-    print(f"{x} is not an integer, dumbass.! Try again.")
-    
+except ValueError:
+    print("x is not an integer, dumbass.! Try again.")
+
 

5e0eac07cbc09f8c4b050ff65e195cc2982f91f8
peacemaker564 <jappikaur17@gmail.com>
2025-11-07 17:53:05 +0000

    Fri, Nov 7, 2025, 11:53 AM -06:00

--- cs50p/numbers.py
+++ cs50p/numbers.py
@@ -1,12 +1,16 @@
-try:
-    x = int(input("What's the x? "))
-
-
-
-except ValueError:
-    print("x is not an integer, dumbass.! Try again.")
-
-else:
+def main():
+    x = getint()
     print(f"x is {x}")
 
+def getint():
+    try:
+        x = int(input("What's the x? "))
+    except ValueError:
+        print("x is not an integer, dumbass.! Try again.")
+
+    else:
+        return x
+
+
+main()
 

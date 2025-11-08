11d33ba07f32aa3386623a46a1a29643a3b93d57
peacemaker564 <jappikaur17@gmail.com>
2025-11-08 16:05:19 +0000

    Sat, Nov 8, 2025, 10:05 AM -06:00

--- indoor.py
+++ indoor.py
@@ -1,2 +1,3 @@
-x = input("What do u wanna print?")
-print(f"{x.lower()}")
+x = input("What do u wanna print?\n")
+print(f"Here is the output in lowercase letter: {x.lower()} ")
+ #using .lower notation to convert upper case to lower case.

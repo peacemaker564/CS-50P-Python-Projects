d9bd4ad76b3077a26cf749408f8726ea3cfce327
peacemaker564 <jappikaur17@gmail.com>
2025-11-09 19:40:02 +0000

    Sun, Nov 9, 2025, 1:40 PM -06:00

--- projects/extensions.py
+++ projects/extensions.py
@@ -5,5 +5,11 @@
 #text/plain
 
 x = input("Enter the file name: ").strip().lower()
-if x.endswith(".gif", ".jpeg" , ".jpg" , ".png")
+if x.endswith(".gif", ".jpeg" , ".jpg" , ".png"):
+    print("image/", x)
+
+elif x.endswith("pdf", "zip"):
+    print("application/", x)
+else:
+    
 

a7e6cd7f8329cbcfdf61bddbc4874495ee851732
peacemaker564 <jappikaur17@gmail.com>
2025-11-09 18:09:34 +0000

    Sun, Nov 9, 2025, 12:09 PM -06:00

--- indoor/bank.py
+++ indoor/bank.py
@@ -1,4 +1,17 @@
 #Bank greeting project.
 
+owed_amount = 0;
 greeting = input("What greeting did u receive? ").strip().lower()
+if greeting == "hello":
+    print("You are owed $", owed_amount)
+
+elif greeting[0] == 'h':
+    owed_amount = 20
+    print("You are owed $", owed_amount)
+
+else:
+    owed_amount = 100
+    print("You are owed $", owed_amount)
+
+
 

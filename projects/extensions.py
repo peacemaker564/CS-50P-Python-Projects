d33ae689c0422d3e7cebf4dcbbaefb1148500fc2
peacemaker564 <jappikaur17@gmail.com>
2025-11-09 18:39:09 +0000

    Sun, Nov 9, 2025, 12:39 PM -06:00

--- projects/extensions.py
+++ projects/extensions.py
@@ -1,3 +1,9 @@
 #This program would accept file datatypes, and present them in their respective MIME Types.
 
+#images/gif,jpg, jpeg, png.
+#application/pdf,zip
+#text/plain
+
+x = input("Enter the file name: ").strip().lower()
+
 

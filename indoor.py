e9b37180dc293d64b5a7fa9ad096940d3d9bb5fb
peacemaker564 <jappikaur17@gmail.com>
2025-11-09 17:25:40 +0000

    Sun, Nov 9, 2025, 11:25 AM -06:00

--- indoor.py
+++
@@ -1,3 +0,0 @@
-x = input("What do u wanna print?\n")
-print(f"Here is the output in lowercase letter: {x.lower()} ")
- #using .lower notation to convert upper case to lower case.
---
+++ indoor/deept.py
@@ -0,0 +1,15 @@
+#“All right,” said the computer, and settled into silence again. The two men fidgeted. The tension was unbearable.
+#“You’re really not going to like it,” observed Deep Thought.
+#“Tell us!”
+#“All right,” said Deep Thought. “The Answer to the Great Question…”
+#“Yes…!”
+#“Of Life, the Universe and Everything…” said Deep Thought.
+#“Yes…!”
+#“Is…” said Deep Thought, and paused.
+#“Yes…!”
+#“Is…”
+#“Yes…!!!…?”
+#“Forty-two,” said Deep Thought, with infinite majesty and calm.”
+
+print("")
+

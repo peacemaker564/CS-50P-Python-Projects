b82d464d479d1c9395f09e130b16dfbaebbe157a
peacemaker564 <jappikaur17@gmail.com>
2025-12-24 04:59:58 +0000

    Tue, Dec 23, 2025, 10:59 PM -06:00

--- projects/coke.py
+++ projects/coke.py
@@ -1,14 +1,16 @@
 #This program asks the user for amount(cost) of a Coke, which costs 50 cents. The machine only accepts 25, 10 and 5
 #cents, every once. When the user enters the amount, pass them the amount due, and if paid more, the amount owed.
-amount_entered = int(input("Enter a coin: "))
+
+
+VALID_COINS = {10, 25, 50}
 amount_due = 50
-VALID_COINS = 
 
 while amount_due > 0:
-        if amount_entered not in (10, 25, 50):
-        print("Not accepted. Please Enter another coin: ")
 
+        amount_entered = int(input("Enter a coin: "))
+        if amount_entered not in VALID_COINTS:
+                print("Not accepted. Please Enter another coin: ")
+                continue
 
-
-
+        amount_due = amount_due - amount_entered
 

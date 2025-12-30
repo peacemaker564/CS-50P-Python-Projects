4f54759c6b7372c1767a2c4499673bbf7eedd89b
peacemaker564 <jappikaur17@gmail.com>
2025-12-30 20:26:11 +0000

    Tue, Dec 30, 2026, 2:26 PM -06:00

--- projects/vanity.py
+++ projects/vanity.py
@@ -3,4 +3,10 @@ numbers = {1, 2, 3, 4, 5, 6, 7 ,8 ,9}
 user_input = input("Plate: ")
 
 while True:
-    if user_input =
+    if ch in user_input > 6:
+        print("Exceeds limits. Try again.")
+        user_input = input("Plate: ")
+
+    else:
+        
+

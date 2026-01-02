8f19b2a1c0d0090cfffd842d7ef155bfddbae320
peacemaker564 <jappikaur17@gmail.com>
2026-01-02 23:45:39 +0000

    Fri, Jan 2, 2026, 5:45 PM -06:00

--- projects/vanity.py
+++ projects/vanity.py
@@ -1,12 +1,35 @@
-numbers = {1, 2, 3, 4, 5, 6, 7 ,8 ,9}
-
-user_input = input("Plate: ")
-
-while True:
-    if ch in user_input > 6:
-        print("Exceeds limits. Try again.")
-        user_input = input("Plate: ")
-
+def main():
+    plate = input("Plate: ")
+    if is_valid(plate):
+        print("Valid")
     else:
+        print("Invalid")
 
 
+def is_valid(s):
+    if not (2 <= len(s) <= 6):
+        return False
+
+    if not s.isalnum():
+        return False
+
+    if not (s[0].isalpha() and s[1].isalpha()):
+        return False
+
+    number_started = False
+    for ch in s[2:]: #cuz we checked the first two characters already.
+        if ch.isdigit():
+            if not number_started:
+
+                if ch == "0":
+                    return False
+                number_started = True
+
+        else:
+            #if number starts, we cannot go back to alphabets.
+            if number_started:
+                return False
+
+    return True
+
+main()

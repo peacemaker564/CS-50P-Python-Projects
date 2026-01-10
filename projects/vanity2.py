5b5319d916fc185c8498a0d7b2190c30c70d293d
peacemaker564 <jappikaur17@gmail.com>
2026-01-10 00:32:41 +0000

    Fri, Jan 9, 2026, 6:32 PM -06:00

---
+++ projects/vanity2.py
@@ -0,0 +1,39 @@
+def main():
+    plate = input("Plate: ")
+    if is_valid(plate):
+        print("Valid")
+    else:
+        print("Invalid")
+
+
+def is_valid(s):
+    if not(2 <= len(s) <= 6): #requirement 1 complete. range confirmed.
+        return False
+
+    if not s.isalnum(): # requirement 2 complete. alphabet or either number confirmed.
+        return False
+
+    if not (s[0].isalpha() and s[1].isalpha()): #requirement 3 complete. First and second values of the user input confirmed as alphabets.
+        return False
+
+    number_started = False #boolean variable for number_starting scenario to further check the other requirements.
+    for ch in s[2:]:
+        if ch.isdigit(): #checks if the 3rd value is a digit or not, and if its 0, return false.
+
+            if not number_started:
+
+                if ch == "0":
+                    return False
+
+        else:
+            number_started = True
+
+
+
+
+
+
+    ...
+
+
+main()

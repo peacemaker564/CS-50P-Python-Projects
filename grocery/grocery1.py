846e375e63d38623c2cbfc175b3e5e4a719fb870
peacemaker564 <jappikaur17@gmail.com>
2026-01-23 21:20:17 +0000

    Fri, Jan 23, 2026, 3:20 PM -06:00

--- grocery/grocery1.py
+++ grocery/grocery1.py
@@ -1,9 +1,19 @@
 items_dict = {}
 
 while True:
-    item = input("")
+    try:
+        item = input("").upper()
+        if item in items_dict:
+            items_dict[item] += 1
 
-    items_dict = {"": ""}
+        else:
+            items_dict[item] = 1
 
+    except EOFError:
+        print("EOFError received, ending the program.")
+        break
+    else:
+        continue
 
-
+for i in sorted(item_dict):
+    print(f"{item_dict[i]} {i}")

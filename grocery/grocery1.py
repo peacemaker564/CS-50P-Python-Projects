32d97214ddc323719c2cb8d61b064d5b693ba334
peacemaker564 <jappikaur17@gmail.com>
2026-01-23 20:48:24 +0000

    Fri, Jan 23, 2026, 2:48 PM -06:00

--- grocery/grocery1.py
+++ grocery/grocery1.py
@@ -1,20 +1,4 @@
-items_list = []
-
-while true:
-    try:
-        item = input("").upper()
-
-        items_list.append(item)
-        if item in items_list:
-            count = count + 1
-
-    except EOFError:
-        print("Exiting program.")
-
-    else:
-        for i in items_list:
-            print(count, )
-
+items_list = {}
 
 
 

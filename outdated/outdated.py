f990e026223dbfda149b33daeec08df69a8ebcae
peacemaker564 <jappikaur17@gmail.com>
2026-01-24 21:45:37 +0000

    Sat, Jan 24, 2026, 3:45 PM -06:00

---
+++ outdated/outdated.py
@@ -0,0 +1,25 @@
+#1 Make a list of months.
+months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+]
+
+def format():
+    date = input("Date: ").strip()
+
+    if date = months
+    month, day, year = date.split("/")
+
+while format():
+
+

e8dbb9419238ee571b3150f7cb58ca202735a45f
peacemaker564 <jappikaur17@gmail.com>
2026-01-24 23:46:49 +0000

    Sat, Jan 24, 2026, 5:46 PM -06:00

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -17,36 +17,40 @@ months = [
 
 
 while True:
-    date = input("Date: ").strip().title()
+    try:
+        date = input("Date: ").strip().title()
 
-    if date.startswith(months):
-        match months:
-            case "January":
-                date = date.replace("January", "1")
-            case "February":
-                date = date.replace("February", "2")
-            case "March":
-                date = date.replace("March", "3")
-            case "April":
-                date = date.replace("April", "4")
-            case "May":
-                date = date.replace("May", "5")
-            case "June":
-                date = date.replace("June", "6")
-            case "July":
-                date = date.replace("July", "7")
-            case "August":
-                date = date.replace("August", "8")
-            case "Septenber":
-                date = date.replace("September", "9")
-            case "October":
-                date = date.replace("October", "10")
-            case "November":
-                date = date.replace("November", "11")
-            case "December":
-                date = date.replace("December", "12")
+        if date.startswith(months):
+            match months:
+                case "January":
+                    date = date.replace("January", "1")
+                case "February":
+                    date = date.replace("February", "2")
+                case "March":
+                    date = date.replace("March", "3")
+                case "April":
+                    date = date.replace("April", "4")
+                case "May":
+                    date = date.replace("May", "5")
+                case "June":
+                    date = date.replace("June", "6")
+                case "July":
+                    date = date.replace("July", "7")
+                case "August":
+                    date = date.replace("August", "8")
+                case "Septenber":
+                    date = date.replace("September", "9")
+                case "October":
+                    date = date.replace("October", "10")
+                case "November":
+                    date = date.replace("November", "11")
+                case "December":
+                    date = date.replace("December", "12")
 
-        date = date.replace(" ", "/").replace(",", "/") #Now, the date looks like, 7/9/1963 (example)
+            date = date.replace(" ", "/").replace(",", "/") #Now, the date looks like, 7/9/1963 (example)
+
+    except ValueError:
+        print("Error detected.")
 
     else:
         print("Please enter date in valid format.")

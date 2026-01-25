77a4c9e79b6844be8d9533e50da248645559eb9f
peacemaker564 <jappikaur17@gmail.com>
2026-01-25 04:42:40 +0000

    Sat, Jan 24, 2026, 10:42 PM -06:00

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -50,10 +50,10 @@ while True:
             date = date.replace(" ", "/")
 
         month, day, year = date.split("/")
-        if day < 0 or day > 31:
+        if int(day) < 0 or int(day) > 31:
             print("Number of days exceeds normal range.")
             continue
-        
+
         new_date = year+"/"+month+"/"+day
 
 

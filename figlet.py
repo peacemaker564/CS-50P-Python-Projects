71be7044f14d16da92e198ba1080ca0638083c2b
peacemaker564 <jappikaur17@gmail.com>
2026-02-16 23:55:37 +0000

    Mon, Feb 16, 2026, 5:55 PM -06:00

--- figlet.py
+++ figlet.py
@@ -1,1 +1,2 @@
-#This program uses
+#This program does the work to display command argument provided by user(using sys.argv) and displaying it out
+#using a specific font(which uses pyfiglet package).

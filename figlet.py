dabb5b3480fb5be7290bbdefc0d665f99ee43bd4
peacemaker564 <jappikaur17@gmail.com>
2026-02-17 03:12:49 +0000

    Mon, Feb 16, 2026, 9:12 PM -06:00

--- figlet.py
+++ figlet.py
@@ -10,7 +10,7 @@ def main():
     x = input("Input: ")
 
     if not (len(sys.argv) == 0 or len(sys.argv) == 2):
-        sys.exit("Input capacity can be 0 or 2.")
+        sys.exit("Input capacity can only be 0 or 2.")
 
     #2. If no argument, using random to give random font outputs.
     if len(sys.argv) == 0:

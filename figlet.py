48f9b6aa5edfaa79a4083bffbf87eff6bf2cd3c7
peacemaker564 <jappikaur17@gmail.com>
2026-02-17 00:09:04 +0000

    Mon, Feb 16, 2026, 6:09 PM -06:00

--- figlet.py
+++ figlet.py
@@ -1,7 +1,7 @@
 #This program does the work to display command argument provided by user(using sys.argv) and displaying it out
 #using a specific font(which uses pyfiglet package).
 #IF NO FONT IS SELECTED, A RANDOM FONT IS CHOSEN.
-
+import sys
 import figlet
 
 def main():
@@ -9,6 +9,6 @@ def main():
 
 
 def fig():
-    x = input("Input: ")
-    
+    x = input("Input: ", sys.argv[1])
+
 

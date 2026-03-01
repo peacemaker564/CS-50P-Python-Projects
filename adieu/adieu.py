2e28346936ee7ac3b8ffa8dea694a9d3aba0d97b
peacemaker564 <jappikaur17@gmail.com>
2026-03-01 20:26:58 +0000

    Sun, Mar 1, 2026, 2:26 PM -06:00

--- adieu/adieu.py
+++ adieu/adieu.py
@@ -24,7 +24,7 @@ def main():
              if i == name_list[-1]:
                 print(" and", i)
 
-        print(i ,sep ="" , end = "")
+        print(i , sep ="" , end = "")
 
     print("\n")
 

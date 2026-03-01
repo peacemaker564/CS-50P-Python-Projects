2e36433df7178b6a05df8c84c8c0aecc010943ec
peacemaker564 <jappikaur17@gmail.com>
2026-03-01 20:34:11 +0000

    Sun, Mar 1, 2026, 2:34 PM -06:00

--- adieu/adieu.py
+++ adieu/adieu.py
@@ -26,7 +26,7 @@ def main():
     #Adding another check field to add an oxford comma if there are more than 2 names.
         elif len(name_list) > 2:
             if i == name_list[-1]:
-                print(",and", i)
+                print(" , and", i)
 
         print(i , sep ="" , end = "")
 

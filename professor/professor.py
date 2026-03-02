2ac00cc5120bbfc52cd475cc0099b901e6fc90a6
peacemaker564 <jappikaur17@gmail.com>
2026-03-02 21:44:27 +0000

    Mon, Mar 2, 2026, 3:44 PM -06:00

--- professor/professor.py
+++ professor/professor.py
@@ -1,1 +1,13 @@
-#Generating ten math problems(x+y)
+#Generating ten math problems(x+y) on the basis of levels(1,2, and 3)
+#Where 1 refers to single digit sums, 2 refers to double digit sums, and 3 to triple.
+
+#Prompting the user to input the level, n.
+#Using that level to determine the difficulty(level) of the problems.
+#Using randint to generate random numbers, for both x and y.
+
+#Saving the sum of the two numbers in to a variable.
+#Prompting the user to provide the answer, and giving them three chances, and then displaying the answer if incorrect.
+#If correct, moving onto the next question.
+#After they are done answering all the questions, displaying the total score out of 10.
+
+

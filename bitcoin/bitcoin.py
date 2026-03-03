67395a9068c5a6734fea21a0eeb83f71eda10d2d
peacemaker564 <jappikaur17@gmail.com>
2026-03-03 04:01:41 +0000

    Mon, Mar 2, 2026, 10:01 PM -06:00

---
+++ bitcoin/bitcoin.py
@@ -0,0 +1,5 @@
+#Using the API Key from coin-cap to get the latest bit-coin price.
+#On the basis of how many bitcoins the user provides via command line argument,
+#outputting the updateed exact price to the output buffer.
+
+import sys

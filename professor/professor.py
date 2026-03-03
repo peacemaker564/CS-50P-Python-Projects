14bbd315ed088d6b08336ef2f122a735a6e2d6f4
peacemaker564 <jappikaur17@gmail.com>
2026-03-03 02:28:22 +0000

    Mon, Mar 2, 2026, 8:28 PM -06:00

--- professor/professor.py
+++ professor/professor.py
@@ -39,7 +39,7 @@ def main():
                     print("EEE") #Mark of a wrong answer.
                     chances =  chances + 1
                     print(x, " + ", y , " = ", end="")
-                    user_answer = int(input(""))
+                    
 
             else:
                 print("Correct answer is: ", sum)

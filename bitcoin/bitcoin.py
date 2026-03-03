8fb0c676979dbcc297706c2c80cfbdaabcd38711
peacemaker564 <jappikaur17@gmail.com>
2026-03-03 04:11:15 +0000

    Mon, Mar 2, 2026, 10:11 PM -06:00

--- bitcoin/bitcoin.py
+++ bitcoin/bitcoin.py
@@ -10,7 +10,11 @@ try:
     if len(sys.argv) != 2:
         sys.exit("No value detected.")
 
-    current_value = requests.get("https://rest.coincap.io/v3/assets/bitcoin?apiKey=53b894acb72bd4ff0eef8be9277eabe37b21efe36d98fc8735f3677372f3c75f")
+    current_data = requests.get("https://rest.coincap.io/v3/assets/bitcoin?apiKey=" \
+    "53b894acb72bd4ff0eef8be9277eabe37b21efe36d98fc8735f3677372f3c75f")
+
+    data = data.json
+
 
 
 except requests.RequestException:

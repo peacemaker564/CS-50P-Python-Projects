91ea969734d8af7d9f41d34c918cbef7e8d0c079
peacemaker564 <jappikaur17@gmail.com>
2026-03-12 23:22:26 +0000

    Thu, Mar 12, 2026, 5:22 PM -06:00

--- api.py
+++ api.py
@@ -3,6 +3,7 @@ import requests
 def main():
     response = requests.get("https://api.artic.edu/api/v1/artworks/search")
     content = response.json()
-    print(content)
+    for artwork in content["data"]:
+        print(f"*{artwork['title']}")
 
 main()

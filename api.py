90818e117482c9286fc518fa2c84000cb394692e
peacemaker564 <jappikaur17@gmail.com>
2026-03-12 23:19:07 +0000

    Thu, Mar 12, 2026, 5:19 PM -06:00

--- api.py
+++ api.py
@@ -1,7 +1,7 @@
 import requests
 
 def main():
-    response = requests.get("https://api.artic.edu/api/vi/artworks/search")
+    response = requests.get("https://api.artic.edu/api/v1/artworks/search")
     content = response.json()
     print(content)
 

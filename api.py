c2f62854e03014ac3c7a016966988af13a0bfcaa
peacemaker564 <jappikaur17@gmail.com>
2026-03-12 23:24:51 +0000

    Thu, Mar 12, 2026, 5:24 PM -06:00

--- api.py
+++ api.py
@@ -1,9 +1,15 @@
 import requests
 
 def main():
-    response = requests.get("https://api.artic.edu/api/v1/artworks/search")
-    content = response.json()
-    for artwork in content["data"]:
-        print(f"*{artwork['title']}")
+    try:
+        response = requests.get("https://api.artic.edu/api/v1/artworks/search")
+        content = response.json()
+
+    except requests.RequestException:
+        print("Probable network issue.")
+
+    else:
+        for artwork in content["data"]:
+            print(f"*{artwork['title']}")
 
 main()

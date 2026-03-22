679ce4b8526d3ef047ad041bd5b4cf27a6a6cf07
peacemaker564 <jappikaur17@gmail.com>
2026-03-22 05:38:35 +0000

    Sat, Mar 21, 2026, 11:38 PM -06:00

--- selfproject1.py
+++ selfproject1.py
@@ -1,3 +1,10 @@
 #Making a program to pull off astrology image of the date data from the nasa website.
 #1. Asking the user for the date of which to grab the image url.
-#2. Using requests.get 
+#2. Using requests.get
+import requests
+
+
+def main():
+    try:
+        date = input("")
+

bed6cf68fef2f8e6e88da6d30e584941b75a77e3
peacemaker564 <jappikaur17@gmail.com>
2026-03-22 05:19:26 +0000

    Sat, Mar 21, 2026, 11:19 PM -06:00

--- api.py
+++ api.py
@@ -2,11 +2,21 @@ import requests
 
 def main():
     try:
-        response = requests.get("https://api.artic.edu/api/v1/artworks/search")
-        content = response.json()
 
-    except requests.RequestException:
-        print("Probable network issue.")
+        print("Search the artworks of artists u like.")
+
+        artist = input("Artist: ")
+
+        response = requests.get(
+            "https://api.artic.edu/api/v1/artworks/search",
+            {"q": artist} #q is the specific keyword mentioned on the UOC website
+                           #to access a particular artists's work.
+            )
+        content = response.json()
+        response.raise_for_status()
+
+    except requests.HTTPError:
+        print("Failed to complete the request.")
 
     else:
         for artwork in content["data"]:

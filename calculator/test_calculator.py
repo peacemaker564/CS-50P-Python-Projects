becfc616aa1eea77cb4baa8bb20085e81cbb2735
peacemaker564 <jappikaur17@gmail.com>
2026-03-27 04:22:33 +0000

    Thu, Mar 26, 2026, 10:22 PM -06:00

--- calculator/test_calculator.py
+++ calculator/test_calculator.py
@@ -4,11 +4,8 @@ def main():
     test_square()
 
 def test_square():
-    if square(2) != 4:
-        print("incorrect value of square 2.")
-
-    if square(3) != 9:
-        print("incorrect value of square 3.")
+    assert square(2) == 4
+    assert square(3) == 9
 
 if __name__ == "__main__":
     main()

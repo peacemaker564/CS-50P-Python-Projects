a424cbe386498dfbd03563347fd5173e200d7451
peacemaker564 <jappikaur17@gmail.com>
2026-03-27 04:24:03 +0000

    Thu, Mar 26, 2026, 10:24 PM -06:00

--- calculator/test_calculator.py
+++ calculator/test_calculator.py
@@ -4,8 +4,13 @@ def main():
     test_square()
 
 def test_square():
-    assert square(2) == 4
-    assert square(3) == 9
+
+    try:
+        assert square(2) == 4
+        assert square(3) == 9
+
+    except AssertionError:
+        print("Error in proposed square function.")
 
 if __name__ == "__main__":
     main()

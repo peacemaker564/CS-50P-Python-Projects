aed1aa3062ab3f49b166b68492496dfa2a12cc50
peacemaker564 <jappikaur17@gmail.com>
2026-04-02 20:21:57 +0000

    Thu, Apr 2, 2026, 2:21 PM -06:00

--- bank/bank.py
+++ bank/bank.py
@@ -4,6 +4,17 @@ def main():
 
 def hello_eval(greet):
     if greet.lower().startswith("hello"):
+        return 0
+    elif greet.lower().startswith("h"):
+        return 20
+
+    else:
         return 100
+
+if __name__ == "__main__":
+    main()
+
+
+
 
 

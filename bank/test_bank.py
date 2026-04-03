2ec4f48cf381ec0794c02f541bc3b0f894d452cc
peacemaker564 <jappikaur17@gmail.com>
2026-04-03 14:23:52 +0000

    Fri, Apr 3, 2026, 8:23 AM -06:00

--- bank/test_bank.py
+++ bank/test_bank.py
@@ -6,7 +6,7 @@ def test_hello():
     assert hello_eval("hello, brah!56") == 0
 
 def test_h():
-    assert hello_eval("Hi, Avi.") == 0
+    assert hello_eval("Hi, Avi.") == 20
     assert hello_eval("hi, brah!56") == 20
     assert hello_eval("How are you? , Avi.") == 20
     assert hello_eval("Hohooho, brah!56") == 20

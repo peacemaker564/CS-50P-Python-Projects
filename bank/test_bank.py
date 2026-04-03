4d9036324511d19e11d92c583490206571d8cefc
peacemaker564 <jappikaur17@gmail.com>
2026-04-03 14:22:52 +0000

    Fri, Apr 3, 2026, 8:22 AM -06:00

---
+++ bank/test_bank.py
@@ -0,0 +1,21 @@
+import pytest
+from bank import hello_eval
+
+def test_hello():
+    assert hello_eval("Hello, Avi.") == 0
+    assert hello_eval("hello, brah!56") == 0
+
+def test_h():
+    assert hello_eval("Hi, Avi.") == 0
+    assert hello_eval("hi, brah!56") == 20
+    assert hello_eval("How are you? , Avi.") == 20
+    assert hello_eval("Hohooho, brah!56") == 20
+
+def test_non():
+    assert hello_eval("Sup ? Avi.") == 100
+    assert hello_eval("Yoo, mate.!") == 100
+
+
+
+
+

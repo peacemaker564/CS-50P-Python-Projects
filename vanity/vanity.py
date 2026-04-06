d143acb7668fda5e33f1d6b2c024b8672b2a3c36
peacemaker564 <jappikaur17@gmail.com>
2026-04-06 04:17:58 +0000

    Sun, Apr 5, 2026, 10:17 PM -06:00

--- vanity/vanity.py
+++ vanity/vanity.py
@@ -1,1 +1,16 @@
-v
+def main():
+    plate = input("Plate: ")
+    if is_valid(plate):
+        print("Valid")
+    else:
+        print("Invalid")
+
+
+def is_valid(s):
+    
+
+
+if __name__ == "__main__":
+    main()
+
+

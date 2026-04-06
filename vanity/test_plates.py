c5734bee1f7dcc0208ff9afe4be42d25915d989c
peacemaker564 <jappikaur17@gmail.com>
2026-04-06 05:26:06 +0000

    Sun, Apr 5, 2026, 11:26 PM -06:00

--- vanity/test_plates.py
+++ vanity/test_plates.py
@@ -6,6 +6,7 @@ import pytest
 def test_alpha():
     assert is_valid("Avineak") == False
     assert is_valid("Singh0") == False
+    assert is_valid("AS2005") == True
 
 def test_numbers():
     assert is_valid("Bro2005") == False
@@ -14,4 +15,5 @@ def test_numbers():
 def test_check():
     assert is_valid("ASR205") == True
     assert is_valid("Jatt40") == True
+
 

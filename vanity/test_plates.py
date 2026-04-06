5abd74503fa2e4a54e0d4fe88e0281c1e41ff9e0
peacemaker564 <jappikaur17@gmail.com>
2026-04-06 05:34:14 +0000

    Sun, Apr 5, 2026, 11:34 PM -06:00

--- vanity/test_plates.py
+++ vanity/test_plates.py
@@ -4,13 +4,13 @@ from plates import is_valid
 import pytest
 
 def test_alpha():
-    assert is_valid("Avineak") == False
+    assert is_valid("Avineak") == True
     assert is_valid("Singh0") == False
     assert is_valid("AS") == True
 
 
 def test_numbers():
-    assert is_valid("Bro2005") == False
+    assert is_valid("Bro2005") == True
     assert is_valid("1982JoJo") == False
 
 def test_check():
@@ -20,7 +20,7 @@ def test_check():
     assert is_valid("A1") == False
     assert is_valid("12") == False
     assert is_valid("1AA") == False
-    assert is_valid("A") == False 
+    assert is_valid("A") == False
     assert is_valid("OUTATIME") == False
 
 

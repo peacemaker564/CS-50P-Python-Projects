f15e9745ab3dc837a88f701c22c65fa62653b8e0
peacemaker564 <jappikaur17@gmail.com>
2026-04-07 19:19:29 +0000

    Tue, Apr 7, 2026, 1:19 PM -06:00

---
+++ test_fuel/fuel.py
@@ -0,0 +1,34 @@
+def main():
+
+
+
+
+def
+while True:
+    try:
+        fraction = input("Fraction: ")
+        x, y = fraction.split("/")
+        x = int(x)
+        y = int(y)
+
+        # Check if X is greater than Y (optional, but standard for this CS50 problem)
+        if x > y:
+            continue
+
+        div = round((x / y) * 100)
+
+    except ValueError:
+        print("Please enter appropriate values.")
+        continue
+    except ZeroDivisionError:
+        print("Unethical fraction. Try again.")
+        continue
+    else:
+        if div <= 1:
+            print("E")
+        elif div >= 99:
+            print("F")
+        else:
+            print(f"{div}%")
+
+        break

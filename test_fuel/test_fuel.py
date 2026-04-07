ca57b13653db8176877afce5a9d1ccc759f68f81
peacemaker564 <jappikaur17@gmail.com>
2026-04-07 20:45:29 +0000

    Tue, Apr 7, 2026, 2:45 PM -06:00

--- test_fuel/test_fuel.py
+++ test_fuel/test_fuel.py
@@ -3,9 +3,9 @@ from fuel import gauge
 from fuel import convert
 
 def test_one():
-    assert convert(1/4) == 25
-    assert convert(1/1) == 100
-    assert convert(0/1) == 0
+    assert convert("1/4") == 25
+    assert convert("1/1") == 100
+    assert convert("0/1") == 0
 
 def test_two():
     assert gauge(25) == "25%"

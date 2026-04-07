794f22421969e0aeebd6e99ef8cf2fc5c81a4a98
peacemaker564 <jappikaur17@gmail.com>
2026-04-07 20:50:52 +0000

    Tue, Apr 7, 2026, 2:50 PM -06:00

--- test_fuel/test_fuel.py
+++ test_fuel/test_fuel.py
@@ -2,24 +2,19 @@ import pytest
 from fuel import gauge
 from fuel import convert
 
-def test_one():
+def test_convert():
     assert convert("1/4") == 25
+    assert convert("1/100") == 1
     assert convert("1/1") == 100
-    assert convert("0/1") == 0
-
-def test_two():
-    assert gauge(25) == "25%"
-    assert gauge(100) == "F"
-    assert gauge(99) == "F"
-
-    assert gauge(1) == "E"
-    assert gauge(0) == "E"
-
-def test_valueError():
-    with pytest.raises("ValueError"):
+    # Test that it raises errors
+    with pytest.raises(ZeroDivisionError):
+        convert("1/0")
+    with pytest.raises(ValueError):
         convert("three/four")
 
-
-def test_ZeroDivisionError():
-    with pytest.raises("ZeroDivError"):
-        convert("1/0")
+def test_gauge():
+    assert gauge(1) == "E"
+    assert gauge(0) == "E"
+    assert gauge(99) == "F"
+    assert gauge(100) == "F"
+    assert gauge(50) == "50%"
